cc84a20b26d9ea623c6d21b436178c31df62cc43
James NF8C <nf8c@nf8c.com>
2023-02-08 20:16:34 -0500

    Updated process to generate Linear Key pads.

--- main.py
+++ main.py
@@ -3,10 +3,20 @@
 # Linear garage door remotes calculator
 # We would love to see it ported to the Flipper
 
-fc = int(input("Enter the facility Code: "))
-tn = int(input("Enter the transmitter number ex. 17316: "))
-bn = int(input("Enter the button number default is 2 for a single button remote: "))
+
+version = input("Wireless keypad or remote?? k(keypad)/r(remote) ").lower()[0]
+
+if version == "r":
+    fc = int(input("Enter the facility Code: "))
+    tn = int(input("Enter the transmitter number ex. 17316: "))
+    bn = int(input("Enter the button number default is 2 for a single button remote: "))
+else:
+    fc = 0
+    bn = 1
+    tn = int(input("Enter an entry code between 1 and 999999: "))
+
 frequency = float(input("Enter the frequency in mHz default 318: "))
+
 
 # Change the frequency to kHz
 frequency = int(frequency * 1000000)
@@ -35,3 +45,4 @@ if a == "y":
             f.write(text)
     except FileNotFoundError:
         print("Error creating the file.")
+        
